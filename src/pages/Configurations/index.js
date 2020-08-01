292a10f863156d86ea83cfbfca1ab1237b6e6f4a
matheusPinheiroP <rawrenete@gmail.com>
2020-08-01 18:52:21 -0300

    add edit profile screen

--- src/pages/Configurations/index.js
+++ src/pages/Configurations/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React from "react";
+import { TouchableWithoutFeedback } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { Container } from './styles';
-import EditProfileHeader from '../../components/EditProfileHeader';
+import {
+  Container,
+  Content,
+  AvatarInput,
+  ChangeAvatarDescription,
+  Form,
+  InputTitle,
+  InputContainer,
+  Input
+} from "./styles";
+import EditProfileHeader from "../../components/EditProfileHeader";
 
 const Configurations = () => {
   return (
     <Container>
       <EditProfileHeader />
+
+      <Content>
+        <TouchableWithoutFeedback>
+          <AvatarInput />
+        </TouchableWithoutFeedback>
+        <ChangeAvatarDescription>
+          Tip to change your profile picture
+        </ChangeAvatarDescription>
+
+        <Form>
+          <InputTitle>Name</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Your name"
+              autoCapitalize="words"
+              autoCorrect={false}
+              returnKeyType="next"
+            />
+            <MaterialIcons name="person-pin" size={20} color="#999" />
+          </InputContainer>
+
+          <InputTitle>Biography</InputTitle>
+          <InputContainer>
+            <Input
+              placeholder="Update your Bio"
+              autoCapitalize="words"
+              autoCorrect={false}
+            />
+            <MaterialIcons name="person-pin" size={20} color="#999" />
+          </InputContainer>
+        </Form>
+      </Content>
     </Container>
   );
-}
+};
 
 export default Configurations;
--- src/pages/Configurations/styles.js
+++ src/pages/Configurations/styles.js
@@ -1,5 +1,52 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { StyleSheet, Platform } from "react-native";
 
-export const Container = styled.View`
-  
+export const Container = styled.KeyboardAvoidingView.attrs({
+  behavior: Platform.OS === "ios" ? "padding" : null,
+})`
+  flex: 1;
+  background: #100f12;
+`;
+
+export const Content = styled.ScrollView``;
+
+export const AvatarInput = styled.Image`
+  width: 140px;
+  height: 140px;
+  border-radius: 70px;
+  align-self: center;
+  margin-top: 30px;
+  background: #fff;
+`;
+
+export const ChangeAvatarDescription = styled.Text`
+  color: #fff;
+  text-align: center;
+  margin-top: 15px;
+`;
+
+export const Form = styled.View`
+  padding: 30px;
+`;
+
+export const InputTitle = styled.Text`
+  color: #999;
+  font-weight: bold;
+`;
+
+export const InputContainer = styled.View`
+  flex-direction: row;
+  border-bottom-width: ${StyleSheet.hairlineWidth};
+  border-color: rgba(255, 255, 255, 0.2);
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: "#999",
+})`
+  height: 48px;
+  font-size: 17px;
+  color: #fff;
+  flex: 1;
 `;
